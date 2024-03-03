ec5e0866c9f3d10fa9647060e076cfb22e29a5d0
Madara Uchiwa <160520345+enockservice12@users.noreply.github.com>
2024-03-03 10:33:31 +0000

    Update notification.js

--- scripts/cmds/notification.js
+++ scripts/cmds/notification.js
@@ -7,7 +7,7 @@ module.exports = {
 		version: "1.6",
 		author: "NTKhang",
 		countDown: 5,
-		role: 2,
+		role: 2, 
 		shortDescription: {
 			vi: "Gửi thông báo từ admin đến all box",
 			en: "Send notification from admin to all box"
@@ -35,7 +35,7 @@ module.exports = {
 		},
 		en: {
 			missingMessage: "Please enter the message you want to send to all groups",
-			notification: "Notification from admin bot to all chat groups (do not reply to this message)",
+			notification: "✦🔮𝗠𝗔𝗗𝗔𝗥𝗔🔮✦| 📚 :",
 			sendingNotification: "Start sending notification from admin bot to %1 chat groups",
 			sentNotification: "✅ Sent notification to %1 groups successfully",
 			errorSendingNotification: "An error occurred while sending to %1 groups:\n%2"
@@ -47,7 +47,7 @@ module.exports = {
 		if (!args[0])
 			return message.reply(getLang("missingMessage"));
 		const formSend = {
-			body: `${getLang("notification")}\n────────────────\n${args.join(" ")}`,
+			body: `${getLang("notification")}\n\n✦[${args.join(" ")}]🌸\n\n━━━━━𝑨𝒍𝒆𝒓𝒕𝒆 𝑨𝒌𝒂𝒕𝒔𝒖𝒌𝒊`,
 			attachment: await getStreamsFromAttachment(
 				[
 					...event.attachments,
@@ -102,3 +102,5 @@ module.exports = {
 		message.reply(msg);
 	}
 };
+
+				
