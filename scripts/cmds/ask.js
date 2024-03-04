dd06a60ad477329773745fbffd69f6d8549f8f7b
Madara Uchiwa <160520345+enockservice12@users.noreply.github.com>
2024-03-04 08:19:27 +0000

    Update ask.js

--- scripts/cmds/ask.js
+++ scripts/cmds/ask.js
@@ -3,8 +3,9 @@ const axios = require('axios');
 const Prefixes = [
   '/ai',
   'Salut',
-  'nemo',
+  'nemoo',
   '+ai',
+  'Wendy',
   'ai',
   'ask',
 'madara',
@@ -31,16 +32,13 @@ module.exports = {
       }
       const prompt = event.body.substring(prefix.length).trim();
    if (!prompt) {
-        await message.reply("✦🔮𝗠𝗔𝗗𝗔𝗥𝗔🔮✦
-                            (⁎˃ᴗ˂⁎) YOUR QUESTIONS");
+        await message.reply("𝗦𝗮𝗹𝘂𝘁 𝗷𝗲𝘂𝗻𝗲(𝗲) 𝗡𝗶𝗻𝗷𝗮 𝗺𝗼𝗻 𝗺𝗮𝗶̂𝘁𝗿𝗲 𝘀𝗲𝗶𝗴𝗻𝗲𝘂𝗿 𝗺𝗮𝗱𝗮𝗿𝗮 𝗺'𝗮 𝗽𝗿𝗼𝗴𝗿𝗮𝗺𝗺𝗲́ 𝗽𝗼𝘂𝗿 𝘁'𝗮𝗶𝗱𝗲𝗿 𝗮𝘃𝗲𝗰 𝘁𝗼𝘂𝘁 𝘁𝗲𝘀 𝗾𝘂𝗲𝘀𝘁𝗶𝗼𝗻𝘀,🔮 𝗟𝗲 𝗿𝗶𝗻𝗻𝗴𝗮𝗻 𝗲́𝘁𝗮𝗻𝘁 𝘂𝗻𝗲 𝘁𝗲𝗰𝗵𝗻𝗶𝗾𝘂𝗲 𝗼𝗰𝘂𝗹𝗮𝗶𝗿𝗲 𝘀𝘁𝗼𝗰𝗸 𝗺𝗮 𝘀𝗮𝗴𝗲𝘀𝘀𝗲🔮");
         return;
       }
 
 
       const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
-      const answer = ' ✦🔮𝗠𝗔𝗗𝗔𝗥𝗔🔮✦
-:/n-------------------------------------/n$
-{ response.data.answer;} 🪔' ;
+      const answer = response.data.answer;
 
  
     await message.reply(answer);
@@ -50,3 +48,4 @@ module.exports = {
     }
   }
 };
+      
